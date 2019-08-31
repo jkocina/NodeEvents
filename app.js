ad2e1e75505d2f8f2ff0d7e8baf4d97bbb8feb38
Jacob Kocina <Jkocina.jr@gmail.com>
2019-08-30 22:36:44 -0500

    installing EJS

--- app.js
+++ app.js
@@ -33,22 +33,29 @@ app.get('/users', (req,res) => {
   ];
 
   res.json(users);
-
 });
 
+//Download Document
 app.get('/download', (req,res) => {
   res.download(path.join(__dirname, '/downloads/LegalityofusingGoogleAnalytics.docx'))
 });
 
+//Redirectors
 app.get('/about', (req,res) => {
   res.redirect('/about.html');
 });
 
+app.get('/index', (req,res) => {
+  res.redirect('/index.html');
+});
+
+//Post handler
 app.post('/subscribe', (req,res) => {
   let name = req.body.name;
   let email = req.body.email;
 
-  console.log(name+' has subscribed with' +email);
+  console.log(name + ' has subscribed with ' + email + '\n');
+  res.redirect('/index.html');
 });
 
 //Looks kinda like a magic method in PHP
@@ -57,6 +64,7 @@ app.get('/users/:name', (req, res) => {
   res.send('<h1>' + user + '</h1>');
 });
 
+//Launch the server
 app.listen(3000, () => {
   console.log("server started on port 3000");
 });
