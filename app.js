0606bc949c8efd244c8ca63a1fb843351471ebc6
Jacob Kocina <Jkocina.jr@gmail.com>
2019-08-30 20:38:41 -0500

    Setting up api

--- app.js
+++ app.js
@@ -6,6 +6,35 @@ const app = express();
 // set static path (middleware)
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/users', (req,res) => {
+  let users = [
+    {
+      first_name: "John",
+      last_name: "Doe",
+      age: "34",
+      gender:"Male"
+    },
+    {
+      first_name: "Bill",
+      last_name: "Paxton",
+      age: "77",
+      gender:"Male"
+    },
+    {
+      first_name: "Stacey",
+      last_name: "Miller",
+      age: "26",
+      gender:"Female"
+    }
+  ];
+
+  res.json(users);
+
+});
+
+app.get('/download', (res,req) => {
+  res.download(path.join(__dirname, '/download/LegalityofusingGoogleAnalytics.docx'))
+})
 /*
 app.get('/', function(req, res) {
   res.send('Hello World');
@@ -15,12 +44,13 @@ app.get('/about', (req, res) => {
   res.send('<h1>About</h1>');
 });
 */
+
 //Looks kinda like a magic method in PHP
 app.get('/users/:name', (req, res) => {
   let user = req.params.name;
   res.send('<h1>' + user + '</h1>');
 });
 
-app.listen(3000, function(){
+app.listen(3000, () => {
   console.log("server started on port 3000");
-})
+});
