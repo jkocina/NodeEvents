a0c34044ed47113de83d5752413feeaf2e203139
Jacob Kocina <Jkocina.jr@gmail.com>
2019-09-09 19:43:52 -0500

    full calendar rendering

--- views/custom/custom.js
+++ views/custom/custom.js
@@ -1,3 +1,12 @@
-(document).ready(function() {
-    let calendar == new fullcalendar
+$(document).ready(function() {
+
+    let calendarEl = document.getElementById('calendar');
+
+    let calendar = new FullCalendar.Calendar(calendarEl, {
+
+        plugins: ['dayGrid']
+
+    });
+
+    calendar.render();
 });
